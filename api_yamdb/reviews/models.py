2db94638f24cf388d18246ffb37555f0a5f99d83
Никита Пискунов <nikpna@yandex.ru>
2023-05-19 22:45:32 +0700

    починил миграции

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -11,7 +11,7 @@ class Сategory(models.Model):
         verbose_name='Подзаголовок',
     )
     name = models.TextField(
-        max_length=256,
+        max_length=100,
         verbose_name='Название категории'
     )
 
